7e993d1d06e5a96056b1fbbbb77e5d431fdf2d5a
orochiZhang <1601265509@qq.com>
2021-06-23 21:17:25 +0800

    修正注释，优化pipeline，request逻辑，删除无用文件

--- vendor/Http/Kernel.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from vendor.Http.Request import Request
-from vendor.Http.Respone import Respone
-
-class Kernel():
-	
-	def Handle(self, env):
-		http_request = Request(env)
-		
